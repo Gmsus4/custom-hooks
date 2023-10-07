ece6176028863e2e711bb990298fd916ffbd296d
Fernando <jesusfgm45@gmail.com>
2023-10-07 11:44:02 -0600

    Primera version

---
+++ useCounter.js
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export const useCounter = (inicialValue = 10) => {
+    const [counter, setCounter] = useState(inicialValue);
+
+    const increment = (value = 1) => {
+        setCounter((current) => current + value);
+    }
+
+    const decrement = (value = 1) => {
+        setCounter((current) => current - value);
+    }
+
+    const reset = () => {
+        setCounter(inicialValue);
+    }
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset
+    }
+}
